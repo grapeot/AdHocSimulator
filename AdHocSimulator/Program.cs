8502ff605aa5ea83bb89f303777e4e0158e01697
Yan Wang <grapeot@gmail.com>
2012-04-29 21:23:03 -0400

    Provide the sample/test code.

--- AdHocSimulator/Program.cs
+++ AdHocSimulator/Program.cs
@@ -9,7 +9,25 @@ namespace grapeot.AdHocSimulator
     {
         static void Main(string[] args)
         {
-            
+            var simulator = new Simulator();
+
+            // initialize two devices and register them to the network
+            var d1 = new Device() { Name = "D1" };
+            var d2 = new Device() { Name = "D2" };
+            d1.ID = simulator.Register(d1, null);
+            d2.ID = simulator.Register(d2, new int[] { d1.ID });
+
+            // define their responses to the received data.
+            var defaultReceivedHandler = new EventHandler<DataReceivedEventArgs>((sender, e) =>
+            {
+                var device = sender as Device;
+                Console.WriteLine("Data received from {0} to {1}.", device.Name, e.TagretDevice.Name);
+            });
+            d1.DataReceived += defaultReceivedHandler;
+            d2.DataReceived += defaultReceivedHandler;
+
+            // test send a large data
+            d1.Send(d2, new byte[20 << 10], () => { Console.WriteLine("Data sent complete from D1 to D2"); });
         }
     }
 }
