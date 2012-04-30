3b2dd186dc76ca9473c37619ae27f5374cff6f51
Yan Wang <grapeot@gmail.com>
2012-04-29 21:11:38 -0400

    Specify the interfaces.

---
+++ AdHocSimulator/Device.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace grapeot.AdHocSimulator
+{
+    /// <summary>
+    /// Class for a mobile device in the Ad-Hoc network.
+    /// </summary>
+    public class Device
+    {
+        /// <summary>
+        /// Gets or sets the ID.
+        /// </summary>
+        /// <value>The ID.</value>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the optional name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Occurs when there is data received. Note due to the network limitation, the data may be incomplete,
+        /// therefore it's Devices' responsibility to do buffer stuffs.
+        /// </summary>
+        /// <remarks>
+        /// Considering the device can only know its neighbor devices, Simulator class shouldn't be invoked in
+        /// the event handler.
+        /// </remarks>
+        public event EventHandler<DataReceivedEventArgs> DataReceived;
+
+        #region Encapsulation of Simulator APIs
+        /// <summary>
+        /// Gets the nearby devices.
+        /// </summary>
+        /// <value>The nearby devices.</value>
+        public Device[] NearbyDevices { get; }
+
+        /// <summary>
+        /// Sends the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="callback">The callback which will be invoked when data sending is finished.</param>
+        public void Send(byte[] data, Action callback = null)
+        {
+            if (callback != null)
+                callback();
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Event arguments for Device.DataReceived.
+    /// </summary>
+    public class DataReceivedEventArgs: EventArgs
+    {
+        public byte[] Data { get; set; }
+    }
+}
---
+++ AdHocSimulator/Simulator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace grapeot.AdHocSimulator
+{
+    public class Simulator
+    {
+        /// <summary>
+        /// Gets or sets the max network speed.
+        /// </summary>
+        /// <value>The max network speed.</value>
+        public int MaxNetworkSpeed { get; set; }
+
+        #region Adjacency APIs
+        List<int> activeIds = new List<int>();
+        List<Tuple<int, int>> adjacentList = new List<Tuple<int, int>>();
+
+        /// <summary>
+        /// Gets the adjacent list, which shows how the devices can reach each other.
+        /// </summary>
+        /// <value>The adjacent list.</value>
+        public List<Tuple<int, int>> AdjacentList { get { return adjacentList; } }
+
+        /// <summary>
+        /// Connects the specified devices.
+        /// </summary>
+        /// <param name="id1">The id1.</param>
+        /// <param name="id2">The id2.</param>
+        public void Connect(int id1, int id2)
+        { }
+
+        /// <summary>
+        /// Connects the specified devices.
+        /// </summary>
+        /// <param name="id1">The id1.</param>
+        /// <param name="id2">The id2.</param>
+        public void Connect(int[] ids1, int[] ids2)
+        { }
+
+        /// <summary>
+        /// Disconnects the specified devices.
+        /// </summary>
+        /// <param name="id1">The id1.</param>
+        /// <param name="id2">The id2.</param>
+        public void Disconnect(int id1, int id2)
+        { }
+
+        /// <summary>
+        /// Disconnects the specified devices.
+        /// </summary>
+        /// <param name="id1">The id1.</param>
+        /// <param name="id2">The id2.</param>
+        public void Disconnect(int[] ids1, int[] ids2)
+        { } 
+        #endregion
+
+        #region Device APIs
+        List<Device> devices;
+        /// <summary>
+        /// Gets the device list.
+        /// </summary>
+        /// <value>The devices.</value>
+        public List<Device> Devices { get; }
+
+        /// <summary>
+        /// Registers the specified device in the network.
+        /// </summary>
+        /// <param name="d">The d.</param>
+        /// <param name="connectedIds">The connected ids.</param>
+        /// <returns>Assigned ID to the device</returns>
+        public int Register(Device d, int[] connectedIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Make specified device leave the network.
+        /// </summary>
+        /// <param name="d">The d.</param>
+        public void Leave(Device d)
+        { } 
+        #endregion
+    }
+}
